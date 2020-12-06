cb711037ec65ab889a27188f43d43306a00ebebe
hzcx998 <2323168280@qq.com>
2020-12-06 22:54:22 +0800

    feat: first init

---
+++ test.py
@@ -0,0 +1,19 @@
+from xtools import file
+from xtools import text
+
+def walk_func(path, arg):
+    print("path name:", path)
+
+if __name__ == "__main__":
+    # walk files
+    file_path = "."
+    file.walk_files(file_path, walk_func, None)
+    xfile = file.load_files(file_path)
+    print(xfile)
+    print(text.remove_website("https://gitee.com abc"))
+    print(text.get_text_charset("test.py"))
+    s = text.convert_list2str([123, 456, 'abcdef'], '\n')
+    print(s)
+    l = text.convert_str2list(s, '\n')
+    print(l)
+    print(file.get_filename("asdasd/asdas//asdasd/xtools"))
